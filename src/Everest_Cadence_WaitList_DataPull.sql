9fac4b82be7633b85dce4e85e6a3f5bd8f3aef94
Jordan Hagan <Jordan.Hagan@gmail.com>
2017-09-25 17:12:29 -0600

    Update Everest_Cadence_WaitList_DataPull.sql

--- src/Everest_Cadence_WaitList_DataPull.sql
+++ src/Everest_Cadence_WaitList_DataPull.sql
@@ -30,8 +30,8 @@ CREATE VOLATILE TABLE VT_DIS_NOTE AS (
 		Left Join PAT_ENC_APPT_MSG APPT on WTL.PAT_ENC_CSN_ID=APPT.PAT_ENC_CSN_ID
 		WHERE 
 		WTL.WAIT_STATUS_C='1' --Pending Status
-		And DEP.DEPARTMENT_NAME in any ('CARD FRNK', 'CARD RKCK', 'CARD SOUT')) as X ) 
-With DATA Primary Index (PK) On Commit Preserve Rows;
+		AND DEP.DEPARTMENT_NAME in any ('CARD FRNK', 'CARD RKCK', 'CARD SOUT')) as X ) 
+With DATA Primary Index (PK) on Commit Preserve Rows;
 
 ------------------------------------------------------------------------------------------------------
 --------------------- CREATE VT_APPT_MES TABLE---------------------------
@@ -39,7 +39,7 @@ With DATA Primary Index (PK) On Commit Preserve Rows;
 CREATE VOLATILE TABLE VT_APPT_MES AS (
 	SELECT Y.*,
 	ROW_NUMBER() OVER (PARTITION BY PK ORDER BY PAT_ENC_CSN_ID) AS RN		
-	FROM(SELECT Distinct 
+	FROM(SELECT DISTINCT 
 		TRIM(WTL.PAT_ENC_CSN_ID) || TRIM(WTL_DP.LINE) as PK, 
 		WTL.PAT_ENC_CSN_ID,
 		WTL.PAT_ID as EPT_CID,
@@ -64,8 +64,8 @@ CREATE VOLATILE TABLE VT_APPT_MES AS (
 		Left Join PAT_ENC_APPT_MSG APPT on WTL.PAT_ENC_CSN_ID=APPT.PAT_ENC_CSN_ID
 		WHERE 
 		WTL.WAIT_STATUS_C='1' --Pending Status
-		And DEP.DEPARTMENT_NAME in any ('CARD FRNK', 'CARD RKCK', 'CARD SOUT')) as Y )
-With DATA Primary Index (PK) On Commit Preserve Rows;
+		AND DEP.DEPARTMENT_NAME in any ('CARD FRNK', 'CARD RKCK', 'CARD SOUT')) as Y )
+With DATA Primary Index (PK) on Commit Preserve Rows;
 ---------------------------------------------------------------------------------------------------------------------------------------------
 /*Test Temp Table, If Needed*/
 --Select * From VT_APPT_MES;
@@ -95,7 +95,7 @@ WITH RECURSIVE REC_TEST (
 	EPT_7296,
 	EPT_7620,
 	LVL) 
-as (Select 
+as (SELECT 
 	PK,
 	PAT_ENC_CSN_ID,
 	EPT_CID,
@@ -109,10 +109,10 @@ as (Select
 	WT_DISPLY_NOTE_EPT_7296(VARCHAR(3000)),
 	APPT_MES_EPT_7620 (VARCHAR(3000)),
 	1
-    From VT_DIS_NOTE
-    Where RN = '1'
+    FROM VT_DIS_NOTE
+    WHERE RN = '1'
 UNION ALL
-    Select 
+    SELECT 
 	PK,
 	PAT_ENC_CSN_ID,
 	EPT_CID,
@@ -126,8 +126,8 @@ UNION ALL
 	TRIM(WT_DISPLY_NOTE_EPT_7296) || ' ' || EPT_7296, 
 	TRIM(APPT_MES_EPT_7620)  || ' ' || EPT_7260,
 	LVL+1
-    From VT_DIS_NOTE 
-    Inner Join REC_TEST on PK = PK_1 and VT_DIS_NOTE.RN = REC_TEST.LVL+1)
+    FROM VT_DIS_NOTE 
+    INNER JOIN REC_TEST on PK = PK_1 and VT_DIS_NOTE.RN = REC_TEST.LVL+1)
 SELECT *
 FROM REC_TEST
 QUALIFY RANK() OVER(PARTITION BY PK_1 ORDER BY LVL DESC) = 1) 
@@ -150,7 +150,7 @@ WITH RECURSIVE REC_TEST (
 	EPT_7296,
 	EPT_7620,
 	LVL) 
-as (Select 
+as (SELECT 
 	PK,
 	PAT_ENC_CSN_ID,
 	EPT_CID,
@@ -164,10 +164,10 @@ as (Select
 	WT_DISPLY_NOTE_EPT_7296(VARCHAR(3000)),
 	APPT_MES_EPT_7620 (VARCHAR(3000)),
 	1
-    From VT_APPT_MES
-    Where RN = '1'
+    FROM VT_APPT_MES
+    WHERE RN = '1'
 UNION ALL
-    Select 
+    SELECT 
 	PK,
 	PAT_ENC_CSN_ID,
 	EPT_CID,
@@ -181,12 +181,12 @@ UNION ALL
 	TRIM(WT_DISPLY_NOTE_EPT_7296) || ' ' || EPT_7296, 
 	TRIM(APPT_MES_EPT_7620)  || ' ' || EPT_7260,
 	LVL+1
-    From VT_APPT_MES 
-    Inner Join REC_TEST on PK = PK_1 and VT_APPT_MES.RN = REC_TEST.LVL+1)
+    FROM VT_APPT_MES 
+    INNER JOIN REC_TEST on PK = PK_1 and VT_APPT_MES.RN = REC_TEST.LVL+1)
 SELECT *
 FROM REC_TEST
 QUALIFY RANK() OVER(PARTITION BY PK_1 ORDER BY LVL DESC) = 1) 
-With DATA Primary Index (PK_1) On Commit Preserve Rows;
+With DATA Primary Index (PK_1) on Commit Preserve Rows;
 ---------------------------------------------------------------------------------------------------------------------------------------------
 /*Test Temp Table, If Needed*/
 --Select * From REC_DIS_NOTE;
@@ -209,5 +209,5 @@ SELECT
 	CASE When MES.EPT_7296=' ' Then NOTE.EPT_7296 Else Null END as NOTE_EPT_7296,
 	CASE When NOTE.EPT_7620=' ' Then MES.EPT_7620 Else Null END as MES_EPT_7620
 FROM REC_DIS_NOTE NOTE
-Inner Join REC_APPT_MES MES on NOTE.PK_1=MES.PK_1 
+INNER JOIN REC_APPT_MES MES on NOTE.PK_1=MES.PK_1 
 ORDER BY NOTE.EPT_1 ASC, NOTE.DP_LINE_NUM ASC
